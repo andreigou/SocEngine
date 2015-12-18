248c75544d7935051f987bd14bbf3c6bcde3c636
AndreiGou <andrei.grosu@list.ru>
2015-12-18 23:46:00 +0300

    Init

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+var pg = require('pg');
+
+pg.connect("postgres://upjfgxnewmzgzo:-hmeZZGe2atZ_jBbguJgbLv6KO@ec2-54-83-36-90.compute-1.amazonaws.com:5432/d2u0mgbchb2mvm?ssl=true", function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client.query('CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))')
+   client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald'])
+    client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese'])
+    
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var pg = require('pg');
+
+pg.connect("postgres://upjfgxnewmzgzo:-hmeZZGe2atZ_jBbguJgbLv6KO@ec2-54-83-36-90.compute-1.amazonaws.com:5432/d2u0mgbchb2mvm?ssl=true", function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client.query('CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))')
+   client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald'])
+    client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese'])
+    client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname")
+
+    .on('row', function(row) {
+     router.get('/', function(req, res, next) {
+  res.render('index', { title: row.firstname });
 });
+});
+});
+/* GET home page. */
+
 
 module.exports = router;
